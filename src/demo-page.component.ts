581f55568d24924046da5c5216063878d78eee0c
Ivan Batic <ivan.batic@live.com>
2016-12-13 17:40:23 +0100

    Updated material select.

--- src/demo-page.component.ts
+++ src/demo-page.component.ts
@@ -2,12 +2,7 @@ import {Component, Input} from "@angular/core";
 @Component({
     selector: "demo-page",
     template: `
-        <ng2-select [placeholder]="'Choose your framework'"
-                [displayBy]="'name'"
-                [selectedDisplayBy]="'label'"
-                [identifyBy]="'name'"
-                [options]="options">
-        </ng2-select>
+        Page Content: {{ }}
     `
 
 
--- src/demo-page.module.ts
+++ src/demo-page.module.ts
@@ -1,13 +1,11 @@
 import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
-import {Ng2SelectModule} from "ng2-material-select/src/ng2-select.module";
 import {DemoPageComponent} from "./demo-page.component";
 
 
 @NgModule({
     imports: [
         CommonModule,
-        Ng2SelectModule
     ],
     declarations: [
         DemoPageModule,
