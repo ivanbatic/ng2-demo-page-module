57f70076a07574ed3695f99716c5e88094791eb2
Ivan Batic <ivan.batic@live.com>
2016-12-13 18:02:05 +0100

    Removed some text

--- src/demo-page.component.ts
+++ src/demo-page.component.ts
@@ -2,8 +2,8 @@ import {Component} from "@angular/core";
 @Component({
     selector: "demo-page",
     template: `
-        Page Content: <ng-content></ng-content>
-        And a button: <demo-button [content]="'Click me'"></demo-button>
+        <ng-content></ng-content>
+        <demo-button [content]="'Click me'"></demo-button>
     `
 
 
